b4a6de6321c7e679638b741abb006384fc7563a2
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2024-11-19 14:41:57 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="threads9703e8c4640bd",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "exorde_data",
+        "selenium==4.2.0",
+        "beautifulsoup==4.12.3",
+        "requests"
+    ],
+    extras_require={"dev": ["pytest", "pytest-cov", "pytest-asyncio"]},
+)
